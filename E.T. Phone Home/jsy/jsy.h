f22154d334175dd2f84ff7dad6a5dbb0f971c214
Yuhsiang Lu <luki0639@gmail.com>
2015-11-25 00:54:42 -0500

    Add very simple interface for jsy interface

--- E.T. Phone Home/jsy/jsy.h
+++ E.T. Phone Home/jsy/jsy.h
@@ -1,9 +1,33 @@
 #ifndef _JSY_H_
 #define _JSY_H_
 
+#include "types.h"
 
+// This is JSY virtual platform library for PC and XBOX
 
+// Error codes for bitstream functions
+typedef enum JSY_ERROR_S {
+    JSY_SUCCEED = 0,
+    JSY_ERROR_OOM = -100,
+} JSY_ERROR_S;
 
+//================================================================================
+// Simple Graphic APIs
 
+// The Graphic handle
+typedef void * JSYGHandle;
+
+// Invalid handle
+#define JSYG_INVALID_HANDLE 0
+
+JSY_ERROR_S JsyGInit();
+
+JSY_ERROR_S JsyGClose();
+
+JSY_ERROR_S JsyGDrawCircle();
+
+JSY_ERROR_S JsyGDrawLine();
+
+JSY_ERROR_S JsyGDrawSprite();
 
 #endif // include guard
---
+++ E.T. Phone Home/jsy/types.h
@@ -0,0 +1,12 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef float float_t;
+typedef unsigned int uint32_t;
+typedef int int32_t;
+typedef char char8_t;
+typedef short int16_t;
+typedef unsigned short uint16_t;
+typedef int bool8_t;
+
+#endif // include guard
